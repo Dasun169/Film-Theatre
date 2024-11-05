eb0071bb5ca9bd35a5fd3d01d040ec8ad78bd06e
Dasun169 <dasunnavindu2001@gmail.com>
2024-11-05 16:23:12 +0530

    booking-page
    complete the html file only.

--- Back-end/FilmTheatre/src/main/java/com/example/FilmTheatre/FilmTheatreApplication.java
+++ Back-end/FilmTheatre/src/main/java/com/example/FilmTheatre/FilmTheatreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmTheatreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FilmTheatreApplication.class, args);
 	}
